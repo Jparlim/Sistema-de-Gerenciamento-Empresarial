6e725f93faa044e54c6b9216b905c2cd832a72e6
Jparlim <Gusjao0500@gmail.com>
2025-11-05 11:08:10 -0300

    reformulando a estrutura

--- src/services/chosenclient/index.ts
+++ src/services/chosenclient/index.ts
@@ -1,10 +1,12 @@
 import { Type } from "@google/genai"
 import { FastifyReply, FastifyRequest } from "fastify"
 import { System } from "../config"
+import prisma from "../../db"
 
 export async function ChosenClient(request:FastifyRequest, reply:FastifyReply) {
-  const {Empresa, NameIA, Instruction, DataName, DataType, TextTitle, Text} = 
-  request.body as { Empresa:string, NameIA:string, Instruction:string, DataName:string, DataType: string | number, TextTitle:string, Text:string }
+  const {Empresa, NameIA, Instruction, DataName, DataType, TextTitle, Text} = request.body as { Empresa:string, NameIA:string, Instruction:string, DataName:string, DataType: string | number, TextTitle:string, Text:string }
+  const { companyId } = request.?user;
+//   vai chegar do JWT o id da empresa no banco
     
     const data = {
         [DataName]: {type: Type[(DataType) as keyof typeof Type]}
@@ -14,9 +16,18 @@ export async function ChosenClient(request:FastifyRequest, reply:FastifyReply) {
         title: TextTitle,
         txt: Text
     }
-    // salvar tudo no banco
 
-    reply.send("ok!")
-    return console.log(text)
-    System(Empresa, NameIA, Instruction, data)
+    // cria banco de dados somente para os textos prontos
+    
+    await prisma.iA.create({
+        data: {
+            company: companyId,
+            nomeEmpresa: Empresa,
+            nomeIA: NameIA,
+            instructions: Instruction,
+            data: data
+        }
+    })
+
+    
 }
--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -1,6 +1,7 @@
 import { GoogleGenAI, Type } from "@google/genai";
 import dotenv from "dotenv";
 import Redis from "ioredis";
+import prisma from "../../db";
 dotenv.config();
 
 const redis = new Redis({
@@ -11,14 +12,30 @@ const redis = new Redis({
 
 const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
-export async function System(Empresa:string, NameIA:string, Instruction:string, Data:object) {
+export async function System(numberClient:number, messageClient:string, empresaNumber:number) {
+
+    const format = {
+        
+    }
+
+    const company = await prisma.company.findUnique({
+        where: {
+            numero: empresaNumber
+        }
+    })
+
+    const datas = await prisma.iA.findMany({
+        where: {
+            id: company?.id
+        }
+    })
     
     const resposta = await ai.models.generateContent({
         model: "gemini-2.5-flash",
-        contents: await redis.get(),
+        contents: ,
         config: {
         responseMimeType: "application/json",
-        systemInstruction: `você é uma atendente da empresa ${Empresa} e seu nome é ${NameIA}, ${Instruction}`,
+        systemInstruction: `você é uma atendente da empresa ${datas[0].nomeEmpresa} e seu nome é ${datas[0].nomeIA}, ${datas[0].instructions}`,
         responseSchema: {
             type: Type.ARRAY,
             items: {
@@ -27,7 +44,7 @@ export async function System(Empresa:string, NameIA:string, Instruction:string, 
                     resposta: {type: Type.STRING},
                     dataClient: {
                         type: Type.OBJECT,
-                        properties: Data
+                        properties: datas[0].data
                     },
                 },
                 propertyOrdering: ["resposta", "dataClient"],
